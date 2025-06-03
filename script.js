14fc58f370d2348e4588d0cc1d9f8159b34ae483
SK Maqshood <skmaqshood99@gmail.com>
2025-06-03 21:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 class Rectangle {
-  constructor(width, height) {
-    this._width = width;
-    this._height = height;
-  }
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
+	get width(){
+		return this._width
+	}
+	get height(){
+		return this._height
+	}
 
-  get width() {
-    return this._width;
-  }
-
-  get height() {
-    return this._height;
-  }
-
-  getArea() {
-    return this._width * this._height;
-  }
+	getArea(){
+		return this._width*this._height
+	}
 }
 
-class Square extends animal {
-  constructor(side) {
-    super(side, side); // Call parent Rectangle constructor with side as both width and height
-  }
-
-  getPerimeter() {
-    return this._width * 4;
-  }
+class Square extends Rectangle {
+	constructor(side){
+		super(side,side);
+		this._side = side;
+	}
+	getPerimeter(){
+		return this._side*4
+	}
 }
+
+// Do not change the code below this line
+window.Rectangle = Rectangle;
+window.Square = Square;
